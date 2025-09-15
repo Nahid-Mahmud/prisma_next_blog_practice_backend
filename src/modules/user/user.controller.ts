a28a42360dad415e1bf7326365d4291839eab292
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-09-15 13:41:53 +0600

    feat: implement user creation logic and response handling

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
+import { userService } from "./user.service";
+import sendResponse from "../../utils/sendResponse";
+import { StatusCodes } from "http-status-codes";
 
 const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  console.log("crate user controller");
+  const data = req.body;
+  // console.log("crate user controller");
+  const response = await userService.createUser(data);
+  sendResponse(res, {
+    success: true,
+    statusCode: StatusCodes.CREATED,
+    data: response,
+    message: "User created Successfully",
+  });
 });
 
 export const userController = {
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,13 @@
-const createUser = async () => {
+import { Prisma, User } from "@prisma/client";
+import prisma from "../../config/db";
+
+const createUser = async (data: Prisma.UserCreateInput): Promise<User> => {
   console.log("create User");
+
+  const res = await prisma.user.create({
+    data: data,
+  });
+  return res;
 };
 
 export const userService = {
